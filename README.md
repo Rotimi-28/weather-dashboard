69b8eb256b5d4d532264624bfa27865e143aac08
Rotimi Rotimi <matanmearoja@yahoo.com>
2022-07-21 16:49:09 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# weather-dashboard
+# weather-current-5dayforecast
+
+## Purpose
+
+The website provides the current and 5 -days forecast for the user provided city.
+* User can search for a city in the search box.
+* Weather data will be fetched using openweathermap api's.
+* The page will have current days weather data including temperature, humidity , wind etc.
+* A 5 day forecast for the city will also be shown.
+* All previously searched cities are also available on the page to choose from.
+* uv index is color coded.
+
+## Built With
+* Html
+* CSS
+* Javascript
+* Jquery
+* Pure CSS
+* Open weather api
+
+## Website
+
+
+
+
+## Contribution
+ By Rotimi-28
---
+++ assets/js/script.js
@@ -0,0 +1,239 @@
+var api_key="d826addc450d06085976d547b5939a0c";
+
+var lon='';
+var lat='';
+var city='';
+
+// function to get the to call the city detail
+
+var getCityDetails = function(city)
+{
+    //This api will take the city string and return the lat/lon
+    
+    var geoCodeUrl= "https://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1&appid="+api_key;
+
+    fetch(geoCodeUrl)
+    .then ( function(response){
+        return response.json();
+    })
+    .then (function(data){  
+        console.log(data);     
+        lat= data[0].lat ;
+        lon= data[0].lon ;
+        if(lat && lon)
+        {
+            
+            //If the user provided a valid city name save the city name for later searches.
+            var cities = JSON.parse(localStorage.getItem("cities"));
+            if(!cities){
+                cities =[];
+            }
+
+
+            //Push the searched city only if it doesn't exist in local storage.
+            if(!cities.some(value => value.toLowerCase() == city.toLowerCase()))
+            {
+                cities.push(city);
+            } 
+            localStorage.setItem("cities", JSON.stringify(cities));
+            getWeatherData();
+            showSearchHistory();        
+        }
+    })    
+    .catch(function (error)
+    {console.log("Error while accessing geo api in openweathermap ,", error);
+        return 0;
+    });
+}
+
+var getWeatherData = function(geoLoc){
+    //This api call will use the latitude and longitude to return current and forecast data.
+    //The web page displays only the current and  daily data. So the api call includes the parameter to exclude minutely, hourly , alerts etc.
+    
+    console.log(lat);
+    console.log(lon);
+    var url = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid="+api_key+"&units=imperial&exclude=minutely,hourly,alerts";
+
+    fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+
+        //Call the following function to display the current weather. Only the current weather details are passed to the function.
+        printCurrentWeather(data.current);
+
+        printForecastWeather(data.daily);       
+    })
+    .catch( function(error){
+        console.log("Errow while calling openweathermap onecall api ,", error);
+    });
+ }
+
+var getColorClass = function(idx){
+    var returnColor ='';
+    var index = parseFloat(idx);
+    //debugger;
+    //Function calculates the class to assign based on the UV index.
+    if (index <= 2)
+    {
+        returnColor ="uv-color-green";
+    }
+    else if(index > 2 && index <= 5 )
+    {
+        returnColor ="uv-color-yellow";
+    }
+    else if(index > 5 && index <= 7)
+    {
+        returnColor ="uv-color-orange";
+    }
+    else if(index > 7 && index <= 10)
+    {
+        returnColor ="uv-color-red";
+    }  
+    return returnColor;
+}
+
+ var printCurrentWeather = function(data){
+    //console.log("Printing icon section",data.weather[0].icon);
+    var weatherIconUrl = "https://openweathermap.org/img/wn/"+data.weather[0].icon+".png";
+    var getWeatherTodayEl = $(".weather-today");
+    var getDate = new Date(data.dt * 1000); 
+    var cityDate = city+' ('+getDate.toLocaleDateString()+') ';
+    var temp = "Temp: "+data.temp;
+    var wind = "Wind: "+data.wind_speed+" mph";
+    var humidity = "Humidity: "+data.humidity+"%";
+    var uvi = data.uvi;   
+
+    var colorCodingUvi =getColorClass(uvi);    
+    console.log(colorCodingUvi);
+
+    var listCityEl = $("<li>")
+                    .addClass("day card-item")
+                    .text(cityDate);    
+    var weatherImage = $("<img>")
+                    .addClass("weathericon");    
+    weatherImage.attr("src", weatherIconUrl);
+    listCityEl.append(weatherImage);
+    var tempEl = $("<li>")
+                    .addClass("card-item")
+                    .text(temp);
+    var tempUnitEl = $("<span>")
+                .html(" &#8457;");
+    tempEl.append(tempUnitEl);
+    var windEl = $("<li>")
+                    .addClass("card-item")
+                    .text(wind);
+    var humidityEl = $("<li>")
+                    .addClass("card-item")
+                    .text(humidity);
+    var uvEl = $("<li>")
+                .addClass("card-item")
+                .text("UV Index:");
+    var spanUvEl = $("<span>")
+                .addClass("uv-index")
+                .addClass(colorCodingUvi)
+                .text(uvi);
+    uvEl.append(spanUvEl);
+    
+    getWeatherTodayEl.append(listCityEl,tempEl,windEl, humidityEl, uvEl);
+ }
+
+ var printForecastWeather = function(data){
+        var forecastHeaderEl= $(".header-forecast");
+        var headerEl = $("<h2>").text("5-Day Forecast:");
+        forecastHeaderEl.append(headerEl);
+
+        var forecastEl = $(".forecast") ;
+        for (var i =0 ; i<5; i++)
+        {
+            var current = data[i];
+            var weatherIconUrl = "https://openweathermap.org/img/wn/"+current.weather[0].icon+".png";
+            var getDate = new Date(current.dt * 1000);  
+            
+            var divCardEl = $("<div>")
+                            .addClass("pure-u-1 pure-u-lg-1-5");
+            
+            var ulEl = $("<ul>")
+                        .addClass("weatherCard");
+              
+            var dateEl = $("<li>")
+                        .addClass("day")
+                        .text(getDate.toLocaleDateString());
+            var iconEl = $("<li>");
+            var weatherImageEl = $("<img>").
+                        addClass("weather-daily-icon");    
+            weatherImageEl.attr("src", weatherIconUrl);
+            iconEl.append(weatherImageEl);
+
+            var tempEl = $("<li>")
+                        .text("Temp: "+current.temp.max);
+            var tempUnitEl = $("<span>")
+                        .html(" &#8457;");
+            tempEl.append(tempUnitEl);
+            var windEl = $("<li>")
+                        .text("Wind: "+current.wind_speed+" Mph");
+            var humidityEl = $("<li>")
+                        .text("Humidity: "+current.humidity+" %");
+
+            ulEl.append(dateEl,iconEl, tempEl, windEl, humidityEl);
+            divCardEl.append(ulEl);
+            forecastEl.append(divCardEl);
+        }
+ }
+ 
+ var showSearchHistory = function(){
+    var cities = JSON.parse(localStorage.getItem("cities"));
+    var ulEl = $('.search-history');
+    ulEl.empty();
+    if(cities)
+    {
+        for(var i=0 ; i< cities.length; i++)
+        {
+            var liEl = $("<li>");
+            var btnEl = $("<button>")
+                        .addClass("pure-button btn");
+            btnEl.text(cities[i]);
+            liEl.append(btnEl);
+            ulEl.append(liEl);
+        }
+    }
+ }
+
+var showData = function(event){
+    event.preventDefault();
+    $(".weather-today").empty();
+    $(".header-forecast").empty();
+    $(".forecast").empty();
+
+    city = $("#search").val().trim();
+      
+    console.log(city);
+
+    if(city)
+    {        
+        getCityDetails(city);   
+    }    
+    $("#search").val('');
+
+ }
+
+ var showDataUsingHistory = function()
+{
+    var el = $(this);
+    $(".weather-today").empty();
+    $(".header-forecast").empty();
+    $(".forecast").empty();
+    city = el.text();
+    getCityDetails(city);  
+}
+
+//Event listener for when user enters a city and clicks search
+$(".search-form").submit(showData);
+
+//Event listener
+$(".search-history").on("click","button",showDataUsingHistory);
+
+//Show search history on page load
+showSearchHistory();
